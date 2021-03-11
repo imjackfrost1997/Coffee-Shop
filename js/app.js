886fa7121a6d3b80ef902313b4ec859d7dbd1bf7
Robert Sampang <maravilloroan@gmail.com>
2021-03-11 22:56:15 +0800

    Layout Finished

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector('.nav');
+const menu = document.querySelector('.menu');
+
+menu.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    menu.classList.toggle('active');
+});
